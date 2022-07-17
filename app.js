b58d03fa7526d7e709861f2b06944d7fd56fc502
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-07-17 17:29:24 -0300

    files structure

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -18,11 +20,8 @@ app.use((req, res, next) => {
   next();
 });
 
+// ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
-// START SERVER
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
